351039b7fb328beb3c16925bd8cb0e1e51418538
Felix Pohl <felix.pohl@brandad-systems.de>
2022-07-20 17:36:31 +0200

    add ghp build; reduce bundle size by removing unused highlighter languages

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,15 +1,19 @@
-import HighlightJS from 'highlight.js';
-import {AfterViewInit, Directive, ElementRef} from "@angular/core";
+import hljs from 'highlight.js/lib/core';
+import typescript from 'highlight.js/lib/languages/typescript';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('xml', xml);
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: 'code[highlight]',
 })
 export class HighlightCodeDirective implements AfterViewInit {
-
-  constructor(private elRef: ElementRef) { }
+  constructor(private elRef: ElementRef) {}
 
   ngAfterViewInit() {
-    HighlightJS.highlightBlock(this.elRef.nativeElement);
+    hljs.highlightBlock(this.elRef.nativeElement);
   }
-
 }

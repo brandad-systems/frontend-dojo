8845cdb3153f9588d5fd37fdf0ac953002a4fe32
Felix Pohl <felix.pohl@brandad-systems.de>
2022-07-25 14:10:52 +0200

    add svg with icon registry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import {Component, HostBinding} from '@angular/core';
-import {FormControl} from "@angular/forms";
+import { Component, HostBinding } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   @HostBinding('class') className = '';
   toggleControl = new FormControl(false);
 
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'github',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/github-circle-white-transparent.svg')
+    );
+  }
   ngOnInit(): void {
     this.toggleControl.valueChanges.subscribe((darkMode) => {
       const darkClassName = 'my-dark-theme';

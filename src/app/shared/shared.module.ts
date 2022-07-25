92ff478d4b0a1f6832850745e5af908622e9f5c1
Felix Pohl <felix.pohl@brandad-systems.de>
2022-07-25 13:28:24 +0200

    link to github project

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,14 +6,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [HighlightCodeDirective],
-  imports: [CommonModule, MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+  ],
   exports: [
     HighlightCodeDirective,
     MatCardModule,
     MatIconModule,
+    MatSlideToggleModule,
     ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule,
